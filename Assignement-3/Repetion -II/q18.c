ea028ab82f1cf4b707700df44a8e0c2c33611932
MUSKAN <muskanprajapati0609@gmail.com>
2024-03-18 22:28:46 +0530

    assignment

---
+++ Assignement-3/Repetion -II/q18.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+//Write a program to print the tables of the numbers from 1 to 10.
+
+int main()
+{
+	printf("Tables from 1 to 10;\n");
+
+	for(int i=1;i<=10;++i)
+	{
+		for(int j=1;j<=10;++j)
+		printf("%d*%d=%d\n",i,j,i*j);
+	}
+//	printf("\n");
+
+
+
+	return 0;
+}
