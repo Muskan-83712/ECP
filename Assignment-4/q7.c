192ebd898fb987af64abc46162d5e151a6b381f4
MUSKAN <muskanprajapati0609@gmail.com>
2024-03-24 09:36:23 +0530

    solution

--- Assignment-4/q7.c
+++ Assignment-4/q7.c
@@ -6,25 +6,25 @@ int fib(int);
 int main()
 {
 	int n;
-//	int t1 =0, t2=1, nextTerm = t1+t2;
 	printf("Enter the numbers: ");
 	scanf("%d",&n);
 
-	printf("%d",fib(n));
+	fib(n);
 
 	return 0;
 }
 int fib(int n)
 {
-	int t1=0, t2=1, nextTerm;
-	printf("Fibonacci series : \n");
+	int t1=0, t2=1 ;
+	printf("Fibonacci series : %d,%d, ",t1,t2);
+	int nextTerm = t1+t2;
 	for (int i=3; i<=n; i++)
 		{
 				printf("%d, ",nextTerm);
 				{
-				t1=t2;
-				t2 = nextTerm;
-				nextTerm = t1+t2;
+					t1=t2;
+					t2 = nextTerm;
+					nextTerm = t1+t2;
 				}
 		}
 }
